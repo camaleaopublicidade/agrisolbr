1b5e705734ea4342beb3a83e045b26d1bee2d2d5
camaleaopublicidade <camaleaoideias@gmail.com>
2025-04-04 18:37:01 -0300

    ce bora porra!

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let swiper = new Swiper(".slide-characters", {
+  slidesPerView: 1.1,
+  spaceBetween: 12,
+  freeMode: true,
+  breakpoints: {
+
+    640: {
+      slidesPerView: 2.1,
+    },
+    768: {
+      slidesPerView: 2.1,
+    },
+    1024: {
+      slidesPerView: 3.1,
+    },
+    1280: {
+      slidesPerView: 3.1,
+    },
+
+    1536: {
+      slidesPerView: 3.1,
+    },
+  },
+});
+
---
+++ js/tailwind.config.js
@@ -0,0 +1,43 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+    },
+    fontFamily: {
+      'body': [
+    'Inter',  
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+    'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
+  }
+}
